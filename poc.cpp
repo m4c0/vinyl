527d42b74e02fa3672df93a9673ce6ccbf528d45
Eduardo Costa <m4c0@users.noreply.github.com>
2025-07-06 14:55:08 +0100

    poc: fix and render something

--- poc.cpp
+++ poc.cpp
@@ -17,9 +17,18 @@ static hai::uptr<sized_stuff> gss {};
 
 static void frame() {
   if (!gss) gss.reset(new sized_stuff {});
+
+  gss->sw.acquire_next_image();
+  gss->sw.queue_one_time_submit(gas->dq.queue(), [&](auto pcb) {
+    gss->sw.cmd_render_pass({
+      .command_buffer = *pcb,
+      .clear_colours { vee::clear_colour(0.1, 0.2, 0.3, 1.0) },
+    });
+  });
+  gss->sw.queue_present(gas->dq.queue());
 }
 
-static struct app_init {
+struct app_init {
   app_init() {
     using namespace vinyl;
     on(START,  [] { gas.reset(new app_stuff {}); });
