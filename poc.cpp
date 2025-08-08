b2dbfc23f2d6122ba87e34b8dcb9bad88aecf038
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-08 14:39:50 +0100

    poc: updating voo

--- poc.cpp
+++ poc.cpp
@@ -7,11 +7,12 @@ import voo;
 
 struct app_stuff {
   voo::device_and_queue dq { "poc-vinyl", casein::native_ptr };
+  vee::render_pass rp = voo::single_att_render_pass(dq);
 };
 static hai::uptr<app_stuff> gas {};
 
 struct sized_stuff {
-  voo::swapchain_and_stuff sw { gas->dq };
+  voo::swapchain_and_stuff sw { gas->dq, *gas->rp };
 };
 static hai::uptr<sized_stuff> gss {};
 
@@ -19,9 +20,9 @@ static void frame() {
   if (!gss) gss.reset(new sized_stuff {});
 
   gss->sw.acquire_next_image();
-  gss->sw.queue_one_time_submit(gas->dq.queue(), [&](auto pcb) {
+  gss->sw.queue_one_time_submit(gas->dq.queue(), [&] {
     gss->sw.cmd_render_pass({
-      .command_buffer = *pcb,
+      .command_buffer = gss->sw.command_buffer(),
       .clear_colours { vee::clear_colour(0.1, 0.2, 0.3, 1.0) },
     });
   });
