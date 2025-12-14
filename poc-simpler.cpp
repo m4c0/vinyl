278f8538fe975aa8c83381676f9181f2fe042c82
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-14 16:19:11 +0000

    poc: extend example to explain why we need fwd-decl

--- poc-simpler.cpp
+++ poc-simpler.cpp
@@ -7,8 +7,10 @@ struct sized_stuff;
 using v = vinyl::v<app_stuff, sized_stuff>;
 
 struct app_stuff {
+  int app_scoped_data = 1;
 };
 struct sized_stuff {
+  int extent_scoped_data = v::as()->app_scoped_data + 1;
 };
 
 const int i = [] {
